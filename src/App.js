55c5ac8f06b5274a9607c1aa26d9bb7c2c9b5922
priyathakurstrix <priyathakur0527@gmail.com>
2022-12-21 22:31:35 +0530

    testing

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useRef, useState } from 'react';
+import { BrowserRouter, Route,Routes } from 'react-router-dom'
+import './App.css';
+import Homepage from './components/pages/Homepage'
+import Buystock from './components/pages/Buystock'
+import Navbar from './components/common/navbar'
+
+// import Footer from './components/common/footer'
+import Pending from './components/pages/pending';
+import Orders from './components/pages/orders';
+import Insights from './components/pages/insights';
+import History from './components/pages/history';
+import Settings from './components/pages/settings';
+
+function App() {
+
+  return (
+   <>
+   <div className='main-wrapper'>
+<BrowserRouter>
+<div className='toggle-btn'><i class="fa-solid fa-bars"></i></div>
+<div className='sticky-nav'>
+
+<Navbar />
+
+</div>
+
+
+<div className='contactsection'>
+<Routes>
+
+<Route path='/' element={<Homepage/>} />
+<Route path='/buystock' element={<Buystock/>} />
+<Route path='/pending' element={<Pending/>} />
+<Route path='/orders' element={<Orders/>} />
+<Route path='/insights' element={<Insights/>} />
+<Route path='/history' element={<History/>} />
+<Route path='/settings' element={<Settings/>} />
+
+</Routes>
+</div>
+</BrowserRouter>
+</div>
+   </>
+  );
+  }
+
+export default App;
